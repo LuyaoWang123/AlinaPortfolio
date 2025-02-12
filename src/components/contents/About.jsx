32e2687c9ec9e7bd4e952123a2ed4921bb0b94f2
LuyaoWang123 <lwang773@wisc.edu>
2025-02-11 21:04:13 -0600

    final button consistency

--- src/components/contents/About.jsx
+++ src/components/contents/About.jsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import '../../styles/Footer.css'
+import '../../styles/About.css'
 import CONTACT_INFO from '../constants';
 
 export default function About() {
@@ -11,11 +11,15 @@ export default function About() {
                     <h1 className='title'>About me</h1>
                     <p className='subtitle'>Alina is a versatile Fullstack Software Engineer with a passion for software design and writing high-quality, maintainable code. She takes a hands-on approach to crafting elegant user interfaces and building robust, stable backend systems. Her expertise includes a keen ability to resolve complex bugs and consistently deliver optimal results. </p>
                     <p className='subtitle'>Currently, Alina is diving into data structures and algorithms (DSA). Curious to see her progress and projects?</p>
-                    <Button 
-                        className="footer-button primary-button"
-                        onClick={() => window.open(CONTACT_INFO.recentWork, '_blank')}
-                        >Check them out!
-                        </Button>         
+                    <div className='check-button-container'>
+                        <div className='check-button-wrapper'>
+                            <Button 
+                            className="check-button"
+                            onClick={() => window.open(CONTACT_INFO.recentWork, '_blank')}
+                            >Check them out!
+                            </Button> 
+                        </div>
+                    </div> 
                 </Col>
             </Row>
             </Container>
